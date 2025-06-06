1ce16d2819fa332c27378ab06c096ffb6e82e88b
VITOR HIDEKI ETO <vitor.eto@etec.sp.gov.br>
2025-06-06 11:42:43 -0300

    06/06

--- dashboard-usuarios/backend/generate.js
+++ dashboard-usuarios/backend/generate.js
@@ -2,10 +2,23 @@ import { faker } from '@faker-js/faker/locale/pt_BR';
 import lodash from 'lodash';
 import fs from 'fs';
 
-const people = lodash.times(50, function(n){
+const peoples = lodash.times(50, function(n){
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
     return {
-        id:
+        id: n + 1,
+        firstName: firstName,
+        lastName: lastName,
+        avatar: faker.image.avatar(),
+        addres: faker.location.streetAddress(),
+        email: faker.internet.email({firstName: firstName.toLowerCase(), lastName: lastName.toLowerCase()})
+
     }
-})
+});
+
+const data = {};
+data.peoples = peoples;
+fs.writeFile('db.json', JSON.stringify(data), (err) => {
+    if (err) throw err;
+    console.log('Finalizando...');
+});
